8a4eb50c982e36c1b6f668dcafb30f67c4724ccc
777dan <danya.nikitin7@gmail.com>
2023-10-14 13:43:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    let age = 0;
+    let speed = 0;
+    let maxSpeed = 0;
+    this.setAge = function (a) {
+        if (a > 0) {
+            age = a;
+        } else {
+            alert("Цей собака, очевидно, ще не з'явився на світ...");
+        }
+    };
+    this.getAge = function () {
+        return age;
+    };
+    this.bark = function () {
+        if (this.weight < 25) {
+            alert(this.name + " сказал Тяф!");
+        } else {
+            alert(this.name + " сказав Гав!");
+        }
+    };
+    this.start = function () {
+        speed = 1;
+    };
+    this.stop = function () {
+        speed = 0;
+    };
+    this.changeSpeed = function (n) {
+        this.start();
+        if (this.weight < 25) maxSpeed = 4;
+        else maxSpeed = 20;
+        if (n >= 0.5 && n <= maxSpeed) {
+            speed *= n;
+        } else {
+            alert("Щось пішло не так...")
+        }
+        alert(speed);
+    };
+    this.toString = function () {
+        return `Name: ${this.name}; Breed: ${this.breed}; Weight: ${this.weight}`;
+    }
+}
+
+let fido = new Dog("Fido", "Mixed", 38);
+let fluffy = new Dog("Fluffy", "Poodle", 30);
+let spot = new Dog("Spot", "Chihuahua", 10);
+
+// fido.setAge(-100);
+let dogs = [fido, fluffy, spot];
+for (let i = 0; i < dogs.length; i++) {
+    // alert(dogs[i].getAge());
+    // dogs[i].bark();
+    // dogs[i].changeSpeed(2);
+    alert(dogs[i]);
+}
+
+if (fido instanceof Dog) {
+    console.log("it's a Dog!");
+};
